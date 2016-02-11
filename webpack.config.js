a753f7bbc47ea5038cf5d48e1bb6d23d20e0fefc
Olivier Combe <olivier.combe@gmail.com>
2016-02-11 21:40:49 +0100

    chore: disabling "mangle" until beta 6
    Fixes #16

--- webpack.config.js
+++ webpack.config.js
@@ -218,7 +218,11 @@ module.exports = function makeWebpackConfig(ENV) {
 
             // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
             // Minify all javascript, switch loaders to minimizing mode
-            new webpack.optimize.UglifyJsPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                // Angular 2 is broken again, disabling mangle until beta 6 that should fix the thing
+                // Todo: remove this with beta 6
+                mangle: false
+            }),
 
             // Copy assets from the public folder
             // Reference: https://github.com/kevlened/copy-webpack-plugin
