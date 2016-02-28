67f9b0dd5f0dc39445c015693d31b9b4ad447a82
Olivier Combe <olivier.combe@gmail.com>
2016-02-28 15:20:23 +0100

    fix: add shims to make IE work again
    Fixes #44

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,4 +1,8 @@
 // Polyfills
+// fixes for non ES6 browsers (IE, safari, ...)
+import 'es6-shim/es6-shim.js';
+// todo remove once https://github.com/angular/angular/issues/6501 is fixed.
+import './shims/shims_for_IE.js';
 import 'angular2/bundles/angular2-polyfills.js';
 
 // Angular 2
