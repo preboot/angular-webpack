14015d1e5da8f81375706220d887d57590c26c55
Charles Fulnecky <charles.fulnecky@hbo.com>
2016-02-28 08:16:01 -0500

    use story reporter

--- karma.conf.js
+++ karma.conf.js
@@ -49,7 +49,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['story', 'coverage'],
 
     // web server port
     port: 9876,
