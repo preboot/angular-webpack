94d8649759e16426b1866f882b1bd14ef40f9755
Redian <redian@users.noreply.github.com>
2016-04-27 08:51:52 +0100

    Update karma.shim.js (#79)
    Some Documentation Update.

--- karma-shim.js
+++ karma-shim.js
@@ -4,9 +4,9 @@ Error.stackTraceLimit = Infinity;
  Ok, this is kinda crazy. We can use the the context method on
  require that webpack created in order to tell webpack
  what files we actually want to require or import.
- Below, context will be an function/object with file names as keys.
+ Below, context will be a function/object with file names as keys.
  using that regex we are saying look in client/app and find
- any file that ends with spec.js and get its path. By passing in true
+ any file that ends with '.spec.ts' and get its path. By passing in true
  we say do this recursively
  */
 var appContext = require.context('./src', true, /\.spec\.ts/);
