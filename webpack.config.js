42d4ee71706b8b63139194752fdc8d173ae9d9bb
PatrickJS <github@gdi2290.com>
2016-05-13 16:37:15 -0700

    chore(webpack.config): use dependency
    remove old code
    
    Closes #98

--- webpack.config.js
+++ webpack.config.js
@@ -262,31 +262,3 @@ function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [__dirname].concat(args));
 }
-
-function rootNode(args) {
-  args = Array.prototype.slice.call(arguments, 0);
-  return root.apply(path, ['node_modules'].concat(args));
-}
-
-function packageSort(packages) {
-  // packages = ['polyfills', 'vendor', 'app']
-  var len = packages.length - 1;
-  var first = packages[0];
-  var last = packages[len];
-  return function sort(a, b) {
-    // polyfills always first
-    if (a.names[0] === first) {
-      return -1;
-    }
-    // main always last
-    if (a.names[0] === last) {
-      return 1;
-    }
-    // vendor before app
-    if (a.names[0] !== first && b.names[0] === last) {
-      return -1;
-    } else {
-      return 1;
-    }
-  }
-}
