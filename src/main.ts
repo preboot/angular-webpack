26811ef8cc32240dca1504719f5086c7df1dacbc
Foxandxss <foxandxss@gmail.com>
2016-07-02 13:56:00 +0200

    chore: update to Angular RC4

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,20 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HTTP_PROVIDERS } from '@angular/http';
 
 import { AppComponent } from './app/app.component';
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 
-const ENV_PROVIDERS = [];
 // depending on the env mode, enable prod mode or add debugging modules
 if (process.env.ENV === 'build') {
   enableProdMode();
-} else {
-  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
 }
 
 bootstrap(AppComponent, [
     // These are dependencies of our App
-    ...HTTP_PROVIDERS,
-    ...APP_ROUTER_PROVIDERS,
-    ...ENV_PROVIDERS,
+    HTTP_PROVIDERS,
+    APP_ROUTER_PROVIDERS,
     { provide: LocationStrategy, useClass: HashLocationStrategy } // use #/ routes, remove this for HTML5 mode
   ])
   .catch(err => console.error(err));
