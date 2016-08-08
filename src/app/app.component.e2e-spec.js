53521c2ae3c7b550217aa6306e8bc6c0069c8ba4
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-08 23:48:32 +0200

    fix e2e tests

--- src/app/app.component.e2e-spec.js
+++ src/app/app.component.e2e-spec.js
@@ -17,7 +17,7 @@ describe('App', function () {
   });
 
   it('should have a main title', function () {
-    expect(element(by.css('main .title')).getText()).toEqual('Hello from Angular 2!');
+    expect(element(by.css('main h1')).getText()).toEqual('Hello from Angular 2!');
   });
 
   it('should have <footer>', function () {
