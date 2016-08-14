6cf47e0cba4f9bb362f743189d91b16e01851d53
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-14 16:27:36 +0200

    chore: add app.component.spec to RC5

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,24 @@
-import {
-  inject,
-  addProviders
-} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { provideRoutes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// to use Translate Service, we need Http, and to test Http we need to mock the backend
-import { BaseRequestOptions, Http } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
-
-// Load the implementations that should be tested
 import { ApiService } from './shared';
 import { AppComponent } from './app.component';
 
 describe('App', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => {
-    addProviders([
-      AppComponent,
-      ApiService,
-      BaseRequestOptions,
-      MockBackend,
-      // Provide a mocked (fake) backend for Http
-      {
-        provide: Http,
-        deps: [MockBackend, BaseRequestOptions],
-        useFactory: function useFactory(backend, defaultOptions) {
-          return new Http(backend, defaultOptions);
-        }
-      }
-    ]);
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [ApiService, provideRoutes([])]
+    });
   });
 
-  it('should have an url', inject([AppComponent], (app: AppComponent) => {
-    expect(app.url).toEqual('https://github.com/preboot/angular2-webpack');
-  }));
+  it('should have an url', () => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    expect(fixture.debugElement.componentInstance.url).toEqual('https://github.com/preboot/angular2-webpack');
+  });
 
 });
