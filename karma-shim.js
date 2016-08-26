e416fd2008e23981cfc1856846d7fdda2d37c703
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-26 16:08:16 +0200

    chore: update to latest zone.js
    Fixes #157

--- karma-shim.js
+++ karma-shim.js
@@ -6,10 +6,12 @@ require('reflect-metadata');
 require('ts-helpers');
 
 require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy');
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/proxy-zone');
 
 /*
  Ok, this is kinda crazy. We can use the the context method on
