bfd239db6304a3440723826ec2d7e6c727e86cf3
monnef <moen@366.hopto.org>
2016-10-27 14:25:41 +0200

    Add SASS file type to be processed by SASS. (#225)

--- webpack.config.js
+++ webpack.config.js
@@ -118,12 +118,12 @@ module.exports = function makeWebpackConfig() {
       // use 'null' loader in test mode (https://github.com/webpack/null-loader)
       // all css in src/style will be bundled in an external css file
       {
-        test: /\.scss$/,
+        test: /\.(scss|sass)$/,
         exclude: root('src', 'app'),
         loader: isTest ? 'null' : ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: ['css', 'postcss', 'sass']})
       },
       // all css required in src/app files will be merged in js files
-      {test: /\.scss$/, exclude: root('src', 'style'), loader: 'raw!postcss!sass'},
+      {test: /\.(scss|sass)$/, exclude: root('src', 'style'), loader: 'raw!postcss!sass'},
 
       // support for .html as raw text
       // todo: change the loader to something that adds a hash to images
